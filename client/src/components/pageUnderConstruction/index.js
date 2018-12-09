2f98c6112fffdec3cf94c1ec09de9326aa830a7b
othman20 <othmansh2010@gmail.com>
2018-12-09 15:25:33 +0200

    add button to move user to the previous page

--- client/src/components/pageUnderConstruction/index.js
+++ client/src/components/pageUnderConstruction/index.js
@@ -2,17 +2,30 @@ import React from 'react';
 import './style.css';
 import Header from '../Header';
 
-const pageUnderConstruction = () => (
-  <React.Fragment>
-    <Header />
-    <section className="notFoundSection">
-      <h2 className="notFoundTitleSub">Page Under Construction</h2>
-      <div className="circle1" />
-      <div className="circle2" />
-      <div className="circle4" />
-      <div className="circle5" />
-    </section>
-  </React.Fragment>
-);
+const pageUnderConstruction = props => {
+  const { history } = props;
+  return (
+    <React.Fragment>
+      <Header />
+      <section className="notFoundSection">
+        <h2 className="notFoundTitleSub">Page Under Construction</h2>
 
+        <button
+          className="notFoundButton"
+          type="button"
+          onClick={() => {
+            history.goBack();
+          }}
+        >
+          go back
+        </button>
+
+        <div className="circle1" />
+        <div className="circle2" />
+        <div className="circle4" />
+        <div className="circle5" />
+      </section>
+    </React.Fragment>
+  );
+};
 export default pageUnderConstruction;
